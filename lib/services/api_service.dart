88565dcbba1de7b1f768f128b550ec2d9377df6d
Luciana Correa <recadoluciana@gmail.com>
2026-05-05 23:11:39 -0300

    pagamento com pix 4

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -838,26 +838,29 @@ class ApiService {
       throw Exception(_mensagemErroAmigavel(e));
     }
   }
-}
 
-Future<Map<String, dynamic>> simularPixPago({required int vendaId}) async {
-  final url = Uri.parse('$baseUrl/pagamentos/pix/sandbox-pay/$vendaId');
+  Future<Map<String, dynamic>> simularPixPago({required int vendaId}) async {
+    final url = Uri.parse('$baseUrl/pagamentos/pix/sandbox-pay/$vendaId');
 
-  debugPrint('POST PIX SANDBOX PAY => $url');
+    debugPrint('POST PIX SANDBOX PAY => $url');
 
-  final response = await http.post(
-    url,
-    headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-  );
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
 
-  debugPrint('STATUS PIX SANDBOX PAY => ${response.statusCode}');
-  debugPrint('RESPOSTA PIX SANDBOX PAY => ${response.body}');
+    debugPrint('STATUS PIX SANDBOX PAY => ${response.statusCode}');
+    debugPrint('RESPOSTA PIX SANDBOX PAY => ${response.body}');
 
-  final data = jsonDecode(response.body);
+    final data = jsonDecode(response.body);
 
-  if (response.statusCode < 200 || response.statusCode >= 300) {
-    throw Exception(data['detail'] ?? 'Erro ao confirmar PIX');
+    if (response.statusCode < 200 || response.statusCode >= 300) {
+      throw Exception(data['detail'] ?? 'Erro ao confirmar PIX');
+    }
+
+    return Map<String, dynamic>.from(data);
   }
-
-  return Map<String, dynamic>.from(data);
 }
